7cb69a89a966b3efb577aa2a56a5e39488bd2ec5
Will Brooke <56451407+redbrooke@users.noreply.github.com>
2023-06-03 13:53:11 +0100

    cleaned up all commands

--- Non-Go/Windows/System-Healthcheck.ps1
+++ Non-Go/Windows/System-Healthcheck.ps1
@@ -9,6 +9,7 @@
 # TODO: Stop using transcript and write to separate files
 # TODO: Re-wording questions, more user friendly. Informed consent. Perhaps for every single command.
 # TODO: After-action sheet to tell users not to run .exes or .ps1 normally.
+# TODO: Find a cleaner way to get the event log stuff
 #
 
 function CreatePopupWindow {
@@ -50,7 +51,6 @@ function currentlyTalking{
 }
 
 function currentlyRunning{
-
       Get-Process 
       Get-Process -FileVersionInfo
       Get-service
@@ -60,102 +60,28 @@ function currentlyRunning{
 function getInstalledStuff{
       Get-CimInstance Win32_StartupCommand | select Name, command, Location, User
       Get-ScheduledTask | select TaskName,State
+      Get-WmiObject -Class Win32_Product
 
 }
 
+function environmentInfo{
+      Get-ComputerInfo
+      Get-ChildItem -Path Env:
+}
 
-function Helpline-Checks {
-  New-Item "~\Documents" -ItemType "directory" -Name "HelplineOutput"    
+function fileNames{
+      Get-ChildItem C:\ -Recurse
+}
 
-  # SYSTEM AND ENVIRONMENT INFO -----------------
-  # Gathers information about the current context we are running on, what and where are we?
-  #
+function usersAndGroups{
+      Get-LocalUser
+      Get-LocalGroup
+}
 
-  if ($environment_approve -eq "y")
-  {
-      Echo "RETRIEVING ENVIRONMENT INFO"
-      Echo "==========================================================================="
-      Echo "Environment variables"
-      Echo "---------------------------------------------------------------------------"
-      gci env:* 	#Display all environment variables
-      Echo "SystemInfo"
-      ECho "---------------------------------------------------------------------------"
-      systeminfo 	#View detailed system configuration information
-      Echo "Patches"
-      Echo "---------------------------------------------------------------------------"
-      wmic qfe 	# Get patches and updates
-      Echo "Installed programs"
-      Echo "---------------------------------------------------------------------------"
-      wmic product get name 	#Get installed programs
-      Get-WmiObject -Class Win32_Product #Get installed programs
-      Echo "Unquoted service paths"
-      Echo "---------------------------------------------------------------------------"
-      wmic service get name,displayname,pathname,startmode | findstr /i "auto" | findstr /i /v "c:\windows\\" | findstr /i /v """"
-      Echo "Tree output (all files)"
-      Echo "---------------------------------------------------------------------------"
-      tree \ /F /A #Outputs a diagram of all files.
-  #Get-ChildItem C:\ -Recurse #Grabs the name and permission of all files.
-  }
+New-Item "~\Documents" -ItemType "directory" -Name "HelplineOutput"    
 
 
-  # PROCESSES AND SERVICES ----------------------
-  # What is runnong on this computer?
-  #
-  if ($process_approve -eq "y")
-  {
-      Echo "RETRIEVING PROCESS INFO"
-      Echo "==========================================================================="
-      Echo "Get-Process output"
-      Echo "---------------------------------------------------------------------------"
-      tasklist /svc 	#Display running processes
-      Get-Process #Display running processes
-      Get-Process -FileVersionInfo 2> $null # Displays more info about the processes that may fail due to lack of permissions.
-      Echo "gci pipes"
-      Echo "---------------------------------------------------------------------------"
-      gci \\.\pipe\   #List named pipes
-      Echo "Checking for scheduled tasks"
-      Echo "---------------------------------------------------------------------------"
-      Get-CimInstance Win32_StartupCommand | select Name, command, Location, User     | fl  #Check startup programs 
-      Get-service # Pulls the name of all services   
-      Get-ScheduledTask | select TaskName,State   #Enumerate scheduled tasks with     PowerShell
-  }
 
-
-  # USERS AND PERMISSIONS --------------------
-  # Who has an account on this computer, and what can everyone do?
-  #
-  if ($users_approve -eq "y")
-  { 
-      Echo "RETRIEVING USER INFO"
-      Echo "==========================================================================="
-      Echo "Whoami permissions"
-      Echo "---------------------------------------------------------------------------"
-      whoami /priv 	#View current user privileges
-      whoami /groups 	#View current user group information
-      Echo "net command output"
-      Echo "---------------------------------------------------------------------------"
-      net user 	#Get all system users
-      net localgroup 	#Get all system groups
-      net localgroup administrators 	#View details about a group
-      Echo "Event Logs"
-      Echo "---------------------------------------------------------------------------"
-      Get-EventLog -List
-      Echo "==========================================================================="
-      Echo "System"
-      Echo "==========================================================================="
-      Get-EventLog -LogName System -Newest 1000
-      Echo "==========================================================================="
-      Echo "Application log"
-      Echo "==========================================================================="
-      Get-EventLog -LogName Application -Newest 1000
-  }
-
-} # end of helpline check function.
-
-
-Start-Job -Name Helpline -ScriptBlock {Helpline-Checks} #Runs approved checks
-Get-Job -Name Helpline | Wait-Job # Waits for all checks to finish before zipping up (some commands may run for longer than this script takes to execute)
-Echo "Compressing"
 
 # This will compress and send the archive. 
 $compress = @{
