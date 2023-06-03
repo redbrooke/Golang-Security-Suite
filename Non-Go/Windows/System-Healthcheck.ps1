49f5b47f7a10dcce1fd2b54afbd279b4d9efdf3b
Will Brooke <56451407+redbrooke@users.noreply.github.com>
2023-06-03 12:15:09 +0100

    started neatening everything into fucntions

--- Non-Go/Windows/System-Healthcheck.ps1
+++ Non-Go/Windows/System-Healthcheck.ps1
@@ -11,47 +11,24 @@
 # TODO: After-action sheet to tell users not to run .exes or .ps1 normally.
 #
 
+#This grabs your WiFi info.
+function howYouConnectToInternet{ 
+      New-Item "~\Documents\HelplineOutput" -ItemType "file" -Name "wifiInfo.txt"
+      Get-NetIPConfiguration | Format-Table -AutoSize | Out-File -append -FilePath ~\Documents\HelplineOutput\wifiInfo.txt
+      Get-NetIPAddress | Format-Table -AutoSize | Out-File -append -FilePath ~\Documents\HelplineOutput\wifiInfo.txt
+      Get-NetNeighbor | Format-Table -AutoSize | Out-File -append -FilePath ~\Documents\HelplineOutput\wifiInfo.txt
+      Get-NetRoute | Format-Table -AutoSize | Out-File -append -FilePath ~\Documents\HelplineOutput\wifiInfo.txt
+}
+
+function currentlyTalking{
+      New-Item "~\Documents\HelplineOutput" -ItemType "file" -Name "liveInfo.txt"
+      Get-NetTCPConnection | Out-File -append -FilePath ~\Documents\HelplineOutput\liveInfo.txt
+      Get-SMBShare | Out-File -append -FilePath ~\Documents\HelplineOutput\liveInfo.txt
+}
 
 
 function Helpline-Checks {
-  New-Item "~\Documents" -ItemType "directory" -Name "HelplineOutput"
-  Start-Transcript -Path "~\Documents\HelplineOutput\TheCyberHelpline.txt" # Outputs everything to a file.
-# WELCOME SECTION -----------------
-  Write-Output "==========================================================================="
-  Write-Output "Welcome! This script aims to give us an overview of your system."
-  $networking_approve = Read-Host "Do you agree to share a snapshot of your networking config? (Connection info, active connections and saved wireless networks) y/n?"
-  $environment_approve = Read-Host "Do you agree to share environment info? (Windows version, patches, installed programs and the names of all files) y/n?"
-  $process_approve = Read-Host "Do you agree to share everything running on your system? (All running processes, Scheduled tasks and named pipes) y/n?"
-  $users_approve = Read-Host "Are you comfortable sharing all accounts and their permissions? (all users, groups and administrative users) y/n?"
-  Write-Output "==========================================================================="
-
-  # NETWORKING SECTION ---------------
-  # Computer networks basically handle sending stuff over the internet (or to other computers). 
-  #
-  if ($networking_approve -eq "y")
-  {
-    Write-Output "RETRIEVING NETWORK INFO"
-    Write-Output "==========================================================================="
-    
-    Write-Output "ipconfig"
-    Write-Output "---------------------------------------------------------------------------"
-    ipconfig /all | Out-File -FilePath ~\Documents\HelplineOutput\network.txt | Wait-Process	# Pulls network info 
-    Write-Output "arp"
-    Write-Output "---------------------------------------------------------------------------"
-    arp -a 	| Out-File -Path ~\Documents\HelplineOutput\arp.txt  #ARP(address resolution protocol) table, links MAC addresses to IPs
-    Write-Output "route print"
-    Write-Output "---------------------------------------------------------------------------"
-    route print 	#Review routing table, basically how you reach different networks
-    Write-Output "Netstat"
-    Write-Output "---------------------------------------------------------------------------"
-    netstat -ano  #Display active network connections
-    Echo "netsh wlan to show wireless profiles"
-    Write-Output "---------------------------------------------------------------------------"
-    netsh wlan show profile   #View saved wireless networks
-    Echo "Show network shares"
-    ECho "---------------------------------------------------------------------------"
-    net share
-  }
+  New-Item "~\Documents" -ItemType "directory" -Name "HelplineOutput"    
 
   # SYSTEM AND ENVIRONMENT INFO -----------------
   # Gathers information about the current context we are running on, what and where are we?
